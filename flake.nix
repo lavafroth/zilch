353fb27c98fdd3eebd84c72463e138daa57477cf
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-10-08 13:25:01 +0530

    ci: nix flake

--- flake.nix
+++ flake.nix
@@ -24,12 +24,9 @@
           libraries = with pkgs; [
             stdenv.cc.cc.lib
             webkitgtk
-            gtk3
-            cairo
-            gdk-pixbuf
             glib
             dbus
-            openssl
+            openssl.dev
             librsvg
           ];
           packages = with pkgs; [
@@ -48,6 +45,7 @@
             pango
             webkitgtk_4_1
             webkitgtk_4_1.dev
+            pkg-config
             # dev tools for frontend
             tailwindcss
             vscode-langservers-extracted
@@ -55,7 +53,13 @@
 
           PKG_CONFIG_PATH =
             with pkgs;
-            "${glib.dev}/lib/pkgconfig:${libsoup_3.dev}/lib/pkgconfig:${webkitgtk_4_1.dev}/lib/pkgconfig:${at-spi2-atk.dev}/lib/pkgconfig:${gtk3.dev}/lib/pkgconfig:${gdk-pixbuf.dev}/lib/pkgconfig:${cairo.dev}/lib/pkgconfig:${pango.dev}/lib/pkgconfig:${harfbuzz.dev}/lib/pkgconfig";
+            "${glib.dev}/lib/pkgconfig:${libsoup_3.dev}/lib/pkgconfig:${webkitgtk_4_1.dev}/lib/pkgconfig:${at-spi2-atk.dev}/lib/pkgconfig:${gtk3.dev}/lib/pkgconfig:${gdk-pixbuf.dev}/lib/pkgconfig:${cairo.dev}/lib/pkgconfig:${pango.dev}/lib/pkgconfig:${harfbuzz.dev}/lib/pkgconfig:${openssl.dev}/lib/pkgconfig";
+          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath libraries}";
+          # https://github.com/tauri-apps/tauri/issues/7354
+          XDG_DATA_DIRS =
+            with pkgs;
+            "${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}:$XDG_DATA_DIRS";
+
         };
       }
     );
