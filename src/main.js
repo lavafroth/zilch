31734b9297a5c6bfa6d1a8a8bb2b8fa65ac8f550
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-10-21 20:38:25 +0530

    feat: gray out uninstall button when selection includes both enabled and disabled packages

--- src/main.js
+++ src/main.js
@@ -27,12 +27,14 @@ let search;
 let selectButton;
 let uninstallButton;
 let disableButton;
-let revertButton;
 
 var packages = [];
 
 var waitViewVisible = true;
 var n_selected = 0;
+
+var disabledCount = 0
+var enabledCount = 0
 
 // same as the keys of `elems`, sacrificing space complexity
 // so that taking subset difference is better than O(n)
@@ -86,6 +88,10 @@ function clear_selection() {
       row.node?.classList.remove('button-select')
   }
   n_selected = 0
+  enabledCount = 0
+  disabledCount = 0
+  uninstallButton.innerHTML = trashSvg
+  uninstallButton.classList.remove('bg-zinc-800')
   ui_selection_mode = false
 }
 
@@ -114,18 +120,70 @@ function gen_row(name, packageId, description) {
   return generateElements(templ)[0]
 }
 
+const trashSvg = `
+<svg class="w-8 h-8 stroke-zinc-300" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="m18 6-.8 12.013c-.071 1.052-.106 1.578-.333 1.977a2 2 0 0 1-.866.81c-.413.2-.94.2-1.995.2H9.994c-1.055 0-1.582 0-1.995-.2a2 2 0 0 1-.866-.81c-.227-.399-.262-.925-.332-1.977L6 6M4 6h16m-4 0-.27-.812c-.263-.787-.394-1.18-.637-1.471a2 2 0 0 0-.803-.578C13.938 3 13.524 3 12.694 3h-1.388c-.829 0-1.244 0-1.596.139a2 2 0 0 0-.803.578c-.243.29-.374.684-.636 1.471L8 6"
+              stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+`;
+
+const revertSvg = `
+    <svg class="w-8 h-8 stroke-zinc-300" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M4 10h13a4 4 0 0 1 4 4v0a4 4 0 0 1-4 4h-5" stroke-width="1.3" stroke-linecap="round"
+        stroke-linejoin="round" />
+      <path d="m7 6-4 4 4 4" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+`;
+const confusedSvg = `
+<svg class="w-8 h-8 stroke-zinc-300" viewBox="100 100 200 200" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M154.604 194.87C152.978 186.642 153.347 178.343 153.347 170.013" stroke="currentColor" stroke-opacity="0.9" stroke-width="16" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M242.371 185.992C244.005 178.565 242.371 170.603 242.371 162.91" stroke="currentColor" stroke-opacity="0.9" stroke-width="16" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M143.005 259.779C154.701 272.112 172.594 288.123 187.201 279.067C201.808 270.012 206.715 251.336 224.438 251.336C242.161 251.336 250.719 260.993 258.639 274.76" stroke="currentColor" stroke-opacity="0.9" stroke-width="16" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M154.605 118C142.523 122.278 132.988 130.833 126 143.664" stroke="currentColor" stroke-opacity="0.9" stroke-width="12" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M239.933 123.799C253.241 132.353 264.298 136.631 273.106 136.631" stroke="currentColor" stroke-opacity="0.9" stroke-width="12" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`;
+
 function toggle_row_focus(row) {
     let node = row.node
     let paragraph = node.children[1]
     if (!selection_mode()) {
       clear_selection()
+      row.selected ^= true;
+      n_selected += row.selected ? 1: -1
+      uninstallButton.innerHTML = row.disabled ? revertSvg : trashSvg;
+      if (row.disabled) {
+        disabledCount = 1
+        enabledCount = 0
+      } else {
+        disabledCount = 0
+        enabledCount = 1
+      }
       node.classList.add('button-select')
       paragraph.classList.toggle('truncate')
     } else {
+      row.selected ^= true;
+      n_selected += row.selected ? 1: -1
+
+      if (row.disabled) {
+        disabledCount += row.selected ? 1: -1
+      } else {
+        enabledCount += row.selected ? 1: -1
+      }
+
+      if (disabledCount > 0 && enabledCount > 0) {
+        uninstallButton.classList.add('bg-zinc-800')
+        uninstallButton.innerHTML = confusedSvg;
+      } else if (disabledCount > 0) {
+        uninstallButton.classList.remove('bg-zinc-800')
+        uninstallButton.innerHTML = revertSvg;
+      } else {
+        uninstallButton.classList.remove('bg-zinc-800')
+        uninstallButton.innerHTML = trashSvg;
+      }
       node.classList.toggle('button-select');
     }
-    row.selected ^= true;
-    n_selected += row.selected ? 1: -1
     status_selection_toggle(selection_mode())
 }
 
@@ -268,7 +326,6 @@ window.addEventListener("DOMContentLoaded", () => {
   selectButton = document.querySelector("#select");
   uninstallButton = document.querySelector("#uninstall");
   disableButton = document.querySelector("#disable");
-  revertButton = document.querySelector("#revert");
 
   selectButton.addEventListener('click', (event) => {
     ui_selection_mode ^= true;
@@ -286,7 +343,10 @@ window.addEventListener("DOMContentLoaded", () => {
         uninstallPkgList.push(pkg.id);
       }
     }
-    uninstallPackages(uninstallPkgList);
+
+    if (!(disabledCount > 0 && enabledCount > 0)) {
+      uninstallPackages(uninstallPkgList);
+    }
   })
 
   search.addEventListener('input', (event) => {
