054ed0ac3e8a3a8181a47a008f98fb8bfefb9c00
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2025-04-02 16:21:18 +0530

    deps: bump flake

--- flake.nix
+++ flake.nix
@@ -1,22 +1,19 @@
 {
-  description = "devshell for github:lavafroth/cabinette";
+  description = "devshell for github:lavafroth/zilch";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs =
     {
-      self,
       nixpkgs,
       flake-utils,
+      ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs {
           system = system;
-          config = {
-            allowUnfree = true;
-          };
         };
       in
       {
