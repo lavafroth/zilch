a72b86231875ac4d232a733d110ec323ba63a7cb
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2025-09-19 12:05:58 +0530

    deps: use `buildInputs` and `nativeBuildInputs` for devshell

--- flake.nix
+++ flake.nix
@@ -17,20 +17,15 @@
         };
       in
       {
-        devShells.default = pkgs.mkShell rec {
-          libraries = with pkgs; [
-            stdenv.cc.cc.lib
-            glib
-            webkitgtk_4_1
-            webkitgtk_4_1.dev
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            pkg-config
             gobject-introspection
-            dbus
-            openssl.dev
-            librsvg
-            libsoup_3
+            cargo
+            cargo-tauri
           ];
-          packages = with pkgs; [
-            # rust backend
+
+          buildInputs = with pkgs; [
             at-spi2-atk
             atkmm
             cairo
@@ -38,18 +33,19 @@
             glib
             gtk3
             harfbuzz
+            librsvg
+            libsoup_3
             pango
-            pkg-config
+            webkitgtk_4_1
+            openssl
+            stdenv.cc.cc.lib
+          ];
+          packages = with pkgs; [
             cargo-tauri
             # dev tools for frontend
             tailwindcss
             vscode-langservers-extracted
           ];
-
-          PKG_CONFIG_PATH =
-            with pkgs;
-            "${glib.dev}/lib/pkgconfig:${libsoup_3.dev}/lib/pkgconfig:${webkitgtk_4_1.dev}/lib/pkgconfig:${at-spi2-atk.dev}/lib/pkgconfig:${gtk3.dev}/lib/pkgconfig:${gdk-pixbuf.dev}/lib/pkgconfig:${cairo.dev}/lib/pkgconfig:${pango.dev}/lib/pkgconfig:${harfbuzz.dev}/lib/pkgconfig:${openssl.dev}/lib/pkgconfig";
-          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath libraries}";
           # https://github.com/tauri-apps/tauri/issues/7354
           XDG_DATA_DIRS = "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS";
         };
