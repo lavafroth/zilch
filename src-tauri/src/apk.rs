321b82f44eb3006b1f3c321b71eaf589d4d47ef4
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2025-09-19 11:56:20 +0530

    fix: remove unwraps for arsc label parsing

--- src-tauri/src/apk.rs
+++ src-tauri/src/apk.rs
@@ -24,10 +24,13 @@ pub fn label(contents: &[u8]) -> Result<Option<String>> {
 
     let xml_root = parse(&manifest).unwrap();
     let arsc = arsc::parse_from(Cursor::new(resource))?;
-    let Node::Element(a) = xml_root.get_root().as_ref().unwrap() else {
-        return Ok(None);
-    };
+    Ok(label_extract_resource_id(xml_root, arsc))
+}
 
+fn label_extract_resource_id(xml_root: axmldecoder::XmlDocument, arsc: arsc::Arsc) -> Option<String> {
+    let Node::Element(a) = xml_root.get_root().as_ref()? else {
+        return None;
+    };
     let Some(attribute) = a.get_children().iter().find_map(|child| {
         if let Node::Element(e) = child {
             e.get_attributes().get("android:label")
@@ -35,19 +38,17 @@ pub fn label(contents: &[u8]) -> Result<Option<String>> {
             None
         }
     }) else {
-        return Ok(None);
+        return None;
     };
 
     let id: usize = attribute
         .strip_prefix("ResourceValueType::Reference/")
         .unwrap_or_default()
-        .parse()?;
+        .parse().ok()?;
     // let package_id = (id >> 24) & 0xff;
     let type_id = ((id >> 16) & 0xff) - 1;
     let index = id & 0xffff;
-    let Some(main_package) = arsc.packages.first() else {
-        return Ok(None);
-    };
+    let main_package = arsc.packages.first()?;
 
     // let verify_type_id = main_package
     //     .type_names
@@ -55,25 +56,18 @@ pub fn label(contents: &[u8]) -> Result<Option<String>> {
     //     .iter()
     //     .position(|s| s == "string")
     //     .unwrap();
-    let Some(typez) = main_package.types.get(type_id) else {
-        return Ok(None);
-    };
 
-    let Some(configs) = typez.configs.iter().find_map(|c| {
+    let configs = main_package.types.get(type_id)?.configs.iter().find_map(|c| {
         c.resources
             .resources
             .iter()
             .find(|r| r.spec_id == index)
             .map(|c| &c.value)
-    }) else {
-        return Ok(None);
-    };
-
+    })?;
     let data_index = match configs {
         arsc::ResourceValue::Plain(p) => p.data_index,
         arsc::ResourceValue::Bag { parent: _, values } => values.first().unwrap().1.data_index,
     };
 
-    let me = arsc.global_string_pool.strings.get(data_index);
-    Ok(me.cloned())
+    arsc.global_string_pool.strings.get(data_index).cloned()
 }
