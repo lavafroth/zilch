7a98499ce6ce40ba8553c0bb1088edaf1e1e4abe
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2025-09-30 17:58:36 +0530

    feat: as_xml_node function for extracting manifest.xml entries

--- src-tauri/src/apk.rs
+++ src-tauri/src/apk.rs
@@ -6,6 +6,13 @@ use zip::ZipArchive;
 pub fn label(contents: &[u8]) -> Result<Option<String>> {
     let cursor = Cursor::new(contents);
     let mut archive = ZipArchive::new(cursor)?;
+    let archive_size_limit = 400 << 20;
+    if archive
+        .decompressed_size()
+        .is_some_and(|estimated_size| estimated_size > archive_size_limit)
+    {
+        return Ok(Some(String::from("No name")));
+    }
 
     let manifest = {
         let entry = "AndroidManifest.xml";
@@ -27,24 +34,25 @@ pub fn label(contents: &[u8]) -> Result<Option<String>> {
     Ok(label_extract_resource_id(xml_root, arsc))
 }
 
-fn label_extract_resource_id(xml_root: axmldecoder::XmlDocument, arsc: arsc::Arsc) -> Option<String> {
-    let Node::Element(a) = xml_root.get_root().as_ref()? else {
-        return None;
-    };
-    let Some(attribute) = a.get_children().iter().find_map(|child| {
-        if let Node::Element(e) = child {
-            e.get_attributes().get("android:label")
-        } else {
-            None
-        }
-    }) else {
-        return None;
-    };
+fn as_xml_element(node: &Node) -> Option<&axmldecoder::Element> {
+    let Node::Element(el) = node else { return None };
+    Some(el)
+}
+
+fn label_extract_resource_id(
+    xml_root: axmldecoder::XmlDocument,
+    arsc: arsc::Arsc,
+) -> Option<String> {
+    let attribute = as_xml_element(xml_root.get_root().as_ref()?)?
+        .get_children()
+        .iter()
+        .find_map(|child| as_xml_element(child)?.get_attributes().get("android:label"))?;
 
     let id: usize = attribute
         .strip_prefix("ResourceValueType::Reference/")
         .unwrap_or_default()
-        .parse().ok()?;
+        .parse()
+        .ok()?;
     // let package_id = (id >> 24) & 0xff;
     let type_id = ((id >> 16) & 0xff) - 1;
     let index = id & 0xffff;
@@ -57,13 +65,18 @@ fn label_extract_resource_id(xml_root: axmldecoder::XmlDocument, arsc: arsc::Ars
     //     .position(|s| s == "string")
     //     .unwrap();
 
-    let configs = main_package.types.get(type_id)?.configs.iter().find_map(|c| {
-        c.resources
-            .resources
-            .iter()
-            .find(|r| r.spec_id == index)
-            .map(|c| &c.value)
-    })?;
+    let configs = main_package
+        .types
+        .get(type_id)?
+        .configs
+        .iter()
+        .find_map(|c| {
+            c.resources
+                .resources
+                .iter()
+                .find(|r| r.spec_id == index)
+                .map(|c| &c.value)
+        })?;
     let data_index = match configs {
         arsc::ResourceValue::Plain(p) => p.data_index,
         arsc::ResourceValue::Bag { parent: _, values } => values.first().unwrap().1.data_index,
