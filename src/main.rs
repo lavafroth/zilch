ea3f42833c73185fac3b0df76aefce6ee78c835c
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2025-12-16 16:47:00 +0530

    feat: basic adb usb connection

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,89 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 #![allow(rustdoc::missing_crate_level_docs)] // it's an example
 
+use adb_client::ADBUSBDevice;
 use eframe::egui;
-use egui::{Button, Layout, Sense, TextEdit};
+use egui::{Button, Layout, RichText, Sense, TextEdit};
 
 fn main() -> eframe::Result {
     // env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        viewport: egui::ViewportBuilder::default().with_inner_size([800.0, 300.0]),
         ..Default::default()
     };
     eframe::run_native(
         "My egui App",
         options,
-        Box::new(|_| Ok(Box::<MyApp>::default())),
+        Box::new(|_| Ok(Box::<App>::default())),
     )
 }
 
-struct MyApp {
-    name: String,
+struct App {
+    search_query: String,
     entries: Vec<Entry>,
+    device: Option<ADBUSBDevice>,
 }
 
 struct Entry {
     id: String,
-    expanded: bool,
+    label: String,
+    expand_triggered: bool,
     selected: bool,
 }
 
-impl Default for MyApp {
+impl Default for App {
     fn default() -> Self {
         Self {
-            name: "someapp".to_owned(),
+            device: None,
+            search_query: "".to_owned(),
 
             entries: vec![
                 Entry {
+                    label: "Ping Pong game".to_string(),
                     id: "ping.pong.bell".to_string(),
-                    expanded: false,
+                    expand_triggered: false,
                     selected: false,
                 },
                 Entry {
+                    label: "Ping Pong game".to_string(),
+                    id: "ping.pong.bell".to_string(),
+                    expand_triggered: false,
+                    selected: false,
+                },
+                Entry {
+                    label: "Ping Pong game".to_string(),
+                    id: "ping.pong.bell".to_string(),
+                    expand_triggered: false,
+                    selected: false,
+                },
+                Entry {
+                    label: "Ping Pong game".to_string(),
+                    id: "ping.pong.bell".to_string(),
+                    expand_triggered: false,
+                    selected: false,
+                },
+                Entry {
+                    label: "Ping Pong game".to_string(),
+                    id: "ping.pong.bell".to_string(),
+                    expand_triggered: false,
+                    selected: false,
+                },
+                Entry {
+                    label: "Ping Pong game".to_string(),
+                    id: "ping.pong.bell".to_string(),
+                    expand_triggered: false,
+                    selected: false,
+                },
+                Entry {
+                    label: "Ping Pong game".to_string(),
+                    id: "ping.pong.bell".to_string(),
+                    expand_triggered: false,
+                    selected: false,
+                },
+                Entry {
+                    label: "Ding Dong game".to_string(),
                     id: "ding.dong.bell".to_string(),
-                    expanded: false,
+                    expand_triggered: false,
                     selected: false,
                 },
             ],
@@ -49,51 +91,62 @@ impl Default for MyApp {
     }
 }
 
-impl eframe::App for MyApp {
+impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let maybe_device = ADBUSBDevice::autodetect();
+        if self.device.is_none() {
+            if let Ok(device) = maybe_device {
+                self.device.replace(device);
+                println!("wow I found a device");
+            } else {
+                println!("looking ...");
+            };
+        }
         egui::CentralPanel::default().show(ctx, |ui| {
             ctx.set_pixels_per_point(1.5);
-            // self.ctrl = ui.input(|i| i.modifiers.ctrl);
 
             ui.take_available_width();
             ui.horizontal(|ui| {
                 ui.take_available_width();
-                let search_label = ui.label("Search: ");
                 ui.add_sized(
                     [ui.available_width(), 20.0],
-                    TextEdit::singleline(&mut self.name),
+                    TextEdit::singleline(&mut self.search_query).hint_text("Search"),
                 )
-                .labelled_by(search_label.id);
             });
 
             ui.separator();
-
-            for entry in self.entries.iter_mut() {
-                render_entry(ui, entry);
-            }
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                for (i, entry) in self.entries.iter_mut().enumerate() {
+                    render_entry(ui, entry, i);
+                }
+            });
         });
     }
 }
 
-fn render_entry(ui: &mut egui::Ui, entry: &mut Entry) {
-    let id = ui.make_persistent_id(&format!("{}_state", entry.id));
+fn render_entry(ui: &mut egui::Ui, entry: &mut Entry, index: usize) {
+    let id = ui.make_persistent_id(format!("{}_state", index));
     let mut state =
         egui::collapsing_header::CollapsingState::load_with_default_open(ui.ctx(), id, false);
 
-    if entry.expanded {
+    if entry.expand_triggered {
         state.toggle(ui);
-        entry.expanded = false;
+        entry.expand_triggered = false;
     }
     state
         .show_header(ui, |ui| {
             ui.with_layout(Layout::top_down_justified(egui::Align::LEFT), |ui| {
-                let response = ui.selectable_label(entry.selected, &entry.id);
-                let id = ui.make_persistent_id(&format!("{}_interact", entry.id));
+                let response = ui.add(
+                    Button::selectable(entry.selected, RichText::new(&entry.label).size(12.0))
+                        .right_text(RichText::new(&entry.id).monospace().size(10.0)),
+                );
+                let id = ui.make_persistent_id(format!("{}_interact", index));
                 if ui
                     .interact(response.rect, id, Sense::click())
                     .double_clicked()
                 {
-                    entry.expanded = true;
+                    entry.expand_triggered = true;
+                    entry.selected ^= true;
                 } else if ui.interact(response.rect, id, Sense::click()).clicked() {
                     entry.selected ^= true;
                 }
