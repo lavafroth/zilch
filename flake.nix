d610947f9dc964e35c2a288e316049ddb039b27f
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2026-08-05 09:39:56 +0530

    flake: add package build derivation

--- flake.nix
+++ flake.nix
@@ -13,6 +13,24 @@
     in
     {
 
+      packages = forAllSystems (pkgs: {
+        default = pkgs.pkgsStatic.rustPlatform.buildRustPackage {
+          pname = "zilch";
+          version = "1.0.0";
+
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+
+          nativeBuildInputs = with pkgs.pkgsStatic; [
+            wayland-protocols
+            wayland
+            libxkbcommon
+            libGL
+            dbus
+
+          ];
+        };
+      });
       devShells = forAllSystems (pkgs: {
 
         default = pkgs.mkShell {
